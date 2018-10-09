26c7a92c5a74bdbd1c3a77290ee369491bdd4b92
tylersiskar <tylersis@buffalo.edu>
2018-10-09 13:56:11 -0400

    Under the Budget window, a spending history button was added. This button can be clicked to access a new window, where a drop down menu of ratings 1 through 5 are available. In the future this will be used to have past expenses, and a rating next to each.

--- main/src/main/java/universityatbuffalo/cse442groupproject/main/BudgetActivity.java
+++ main/src/main/java/universityatbuffalo/cse442groupproject/main/BudgetActivity.java
@@ -3,18 +3,28 @@ package universityatbuffalo.cse442groupproject.main;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class BudgetActivity extends AppCompatActivity {
     int inputbugdet;
+    int inputrating;
     Button submitButton;
+    Button spendingHistory;
+    //EditText ratinginput;
     EditText budgetinput;
+    Spinner ratingvalues;
+    String[] values = new String[]{"1", "2", "3", "4", "5"};
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, values);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget);
+
+
         budgetinput = findViewById(R.id.budgettext);
         submitButton = findViewById(R.id.submitbutton);
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +34,18 @@ public class BudgetActivity extends AppCompatActivity {
                 showToast(String.valueOf(inputbugdet));
             }
     });
+        //ratinginput = findViewById(R.id.ratingtext);
+
+        spendingHistory = findViewById(R.id.historybutton);
+        spendingHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View t) {
+               ratingvalues = findViewById(R.id.ratingvalues);
+               ratingvalues.setAdapter(adapter);
+//                inputrating = Integer.valueOf(ratinginput.getText().toString());
+//                showToast(String.valueOf(inputrating));
+            }
+            });
 
     }
 
