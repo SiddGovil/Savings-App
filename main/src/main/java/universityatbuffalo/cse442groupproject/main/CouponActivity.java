be78eead50fbe5e32b9b6d7789c5fcce0a675a42
krapivani <krapivan@buffalo.edu>
2018-10-16 14:36:49 -0400

    Changed layout, moved around buttons so it looks nicer.

---
+++ main/src/main/java/universityatbuffalo/cse442groupproject/main/CouponActivity.java
@@ -0,0 +1,13 @@
+package universityatbuffalo.cse442groupproject.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CouponActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coupon);
+    }
+}
